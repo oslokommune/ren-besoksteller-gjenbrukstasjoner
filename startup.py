76ab287d27b51fb92203a1c9ffa254d002da8703
oyvind.nygard@knowit.no <oyvind.nygard@knowit.no>
2019-11-25 17:09:09 +0100

    fix startup file

--- startup.py
+++ startup.py
@@ -5,12 +5,12 @@ while True:
     try: 
         print ("Starter snart")
         time.sleep(5)
-        exec(open('/home/pi/ren/kioVisits/kioVisits.py').read())
+        exec(open('/home/pi/ren-besoksteller-gjenbrukstasjoner/kioVisits.py').read())
     
     except Exception as e: 
         print ("Error:")
         print (e)
         print("Restarter...")
         
-        os.execv(sys.executable, ['python3'] + sys.argv) 
+        os.execv(sys.executable, ['python3.7'] + sys.argv)
         continue
